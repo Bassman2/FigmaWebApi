d0ff083754a2624834b7381a731350870353ffe9
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-08-19 17:58:59 +0200

    Update

---
+++ Test/FigmaWebApiUnitTestShare/FigmaBaseUnitTest.cs
@@ -0,0 +1,8 @@
+﻿namespace FigmaWebApiUnitTest;
+
+public abstract class FigmaBaseUnitTest
+{
+    protected const string storeKey = "Figma";
+    
+    protected const string appName = "UnitTest";
+}
---
+++ Test/FigmaWebApiUnitTestShare/FigmaVersionUnitTest.cs
@@ -0,0 +1,16 @@
+﻿namespace FigmaWebApiUnitTest;
+
+[TestClass]
+public class FigmaVersionUnitTest : FigmaBaseUnitTest
+{
+    [TestMethod]
+    public async Task TestMethodGetUserAsync()
+    {
+        using var Figma = new Figma(storeKey, appName);
+
+        var health = await Figma.GetHealthAsync();
+
+        Assert.IsNotNull(health, "Figma version is null");
+        Assert.AreEqual(new Version(11,6,1), new Version(health.Version!), nameof(health.Version));
+    }
+}
