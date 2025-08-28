09f93de18e43f39baf741f0be0a2313bf1a09793
kersRalf Beckers <ralf.beckers@elektrobit.com>
2025-08-28 18:28:35 +0200

    Update

--- Src/FigmaWebApiShare/Figma.cs
+++ Src/FigmaWebApiShare/Figma.cs
@@ -1,4 +1,7 @@
-﻿namespace FigmaWebApi;
+﻿using System.Xml;
+using System.Xml.XPath;
+
+namespace FigmaWebApi;
 
 public class Figma : JsonService
 {
@@ -11,13 +14,28 @@ public class Figma : JsonService
     //protected override string? AuthenticationTestUrl => "v1/me";
 
 
-    //public override async Task<string?> GetVersionStringAsync(CancellationToken cancellationToken = default)
-    //{
-    //    WebServiceException.ThrowIfNotConnected(client);
+    public override async Task<string?> GetVersionStringAsync(CancellationToken cancellationToken = default)
+    {
+        WebServiceException.ThrowIfNotConnected(client);
 
-    //    var res = await GetFromJsonAsync<HealthModel>("api/health", cancellationToken);
-    //    return res?.Version;
-    //}
+        var relNotes = await GetStringAsync("https://www.figma.com/de-de/release-notes/feed/atom.xml", cancellationToken);
+
+        if (relNotes == null)
+            return "0.0.0.0";
+
+        var doc = XDocument.Parse(relNotes);
+        //var list = doc.XPathSelectElements("feed/entry/updated").Select(e => e.Value).ToList();
+
+        XNamespace atom = "http://www.w3.org/2005/Atom";
+
+        var nsManager = new XmlNamespaceManager(new NameTable());
+        nsManager.AddNamespace("atom", atom.NamespaceName);
+
+        var date = doc.XPathSelectElements("//atom:entry/atom:updated", nsManager).
+            Select(u => DateTime.Parse(u.Value, null, System.Globalization.DateTimeStyles.RoundtripKind).Date).Max();
+
+        return $"{date.Year}.{date.Month}.{date.Day}";
+    }
 
     
     public async Task<User?> GetUserAsync(CancellationToken cancellationToken = default)
